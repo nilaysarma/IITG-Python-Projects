57b3a3d4925f33d3f8d5f8b28392dd5bad2bd496
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-01-19 10:47:43 +0530

    add AgeCalculator

---
+++ AgeCalculator.py
@@ -0,0 +1,22 @@
+# Week 2 Lab 2: Age Calculator with Birthday Countdown
+
+import datetime
+
+Date_of_Birth = input("Enter your Date of Birth (DD-MM-YYYY): ")
+
+Date_of_Birth = datetime.datetime.strptime(Date_of_Birth, "%d-%m-%Y").date()
+Current_Date = datetime.date.today()
+age = Current_Date.year - Date_of_Birth.year
+
+print("Your Age is: ", age)
+
+Next_Birthday = Date_of_Birth.replace(year=Current_Date.year)
+if(Next_Birthday == Current_Date):
+    print("Today is your birthday. Happy Birthday!")
+    exit()
+if Next_Birthday < Current_Date:
+    Next_Birthday = Next_Birthday.replace(year=Current_Date.year + 1)
+Days_Left = Next_Birthday - Current_Date
+
+print("Your next birthday is on: ", Next_Birthday.strftime("%d-%m-%Y"))
+print(Days_Left.days, " days left for your next birthday")
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# IITG-Python-Projects
+# IITG Python Projects
+Get all Year 1 Trimester 2 Python Lab projects in this repository.
+
+## Get Started
+Clone this repository to get started!
+```sh
+git clone https://github.com/nilaysarma/IITG-Python-Projects.git
+```
+
+## Run a Python file
+To run a Python file, use this command
+
+On Windows
+```
+python FILE_NAME.py
+```
+On Linux & MacOS
+```
+python3 FILE_NAME.py
+```
