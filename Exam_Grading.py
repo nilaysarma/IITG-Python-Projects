0de778d3ee29aaeda5a0dbd2b2309cbfa0a13739
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-02-08 09:12:50 +0530

    add some files

---
+++ Exam_Grading.py
@@ -0,0 +1,7 @@
+# Draft
+
+name = input("Enter Student name: ")
+roll = int(input("Enter Roll No. "))
+maths_score = int(input("Enter marks in Maths: "))
+physics_score = int(input("Enter marks in Physics: "))
+chemistry_score = int(input("Enter marks in Chemistry: "))
