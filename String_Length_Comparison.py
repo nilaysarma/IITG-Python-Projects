4bd2d51141399ed3f060e332492c80105be7a309
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-02-08 10:05:58 +0530

    add proper comments

--- String_Length_Comparison.py
+++ String_Length_Comparison.py
@@ -1,1 +1,2 @@
-# Draft
+# Week 4 Lab: String Length Comparison
+# Description: Prompt the user to enter two strings. Write a Python program to compare the lengths of the two strings and print a message indicating which string is longer, or if they are of equal length.
--- Temperature_Converter.py
+++ Temperature_Converter.py
@@ -1,1 +1,2 @@
-# Draft
+# Week 4 Lab: Temperature Converter
+# Description: a Python program that prompts the user to enter a temperature and its unit (Celsius or Fahrenheit).Based on the unit provided, convert the temperature to the other unit and display the result. Use a string variable to store the unit.
