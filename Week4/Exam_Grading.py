6f3ddca45c2440c50d52ecaed82aa9116b8fc1c5
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-02-14 18:57:49 +0530

    Update Exam Grading

--- Week4/Exam_Grading.py
+++ Week4/Exam_Grading.py
@@ -1,7 +1,7 @@
 # Week 4 Lab: Exam Grading
 # Description: A Python Program which asks the instructor to enter a student's details (namely, Name, Roll Number), and the score secured by the student in three subjects (namely in Physics, Chemistry and Maths). The program should then process this data and compute the grade for each subject and also the overall grade. The output should be presented in the terminal as neatly structured like a grade card.
 
-print("--------------------")
+print("-" * 20)
 name = input("Enter Student name: ")
 roll = int(input("Enter Roll No. "))
 maths_score = float(input("Enter marks in Maths: "))
@@ -25,12 +25,12 @@ physics_grade = grade(physics_score)
 chemistry_grade = grade(chemistry_score)
 overall_grade = grade((maths_score + physics_score + chemistry_score) / 3)
 
-print("--------------------\nHere is the Grade Card:\n--------------------")
+print("-"  * 20 + "\nHere is the Grade Card:\n" + "-" * 20)
 print("Student Name: ", name)
 print("Student Roll: ", roll)
 print("Grade in Maths: ", maths_grade)
 print("Grade in Physics: ", physics_grade)
 print("Grade in Chemistry: ", chemistry_grade)
 print("Overall Grade: ", overall_grade)
-print("--------------------")
+print("-" * 20)
 print("Thank you!")
