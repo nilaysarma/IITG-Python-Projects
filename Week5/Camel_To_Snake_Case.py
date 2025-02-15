8a64950a1642f8409e6e72551de69d64c809cfc5
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-02-15 08:50:14 +0530

    Modify Camel case to Snake case

--- Week5/Camel_To_Snake_Case.py
+++ Week5/Camel_To_Snake_Case.py
@@ -1,3 +1,6 @@
+# Week 5 Lab: Camel case to Snake case
+# Description: When texting, it’s not uncommon to shorten words to save time or space, as by omitting vowels. In a file called shorten_message.py, implement a program that prompts the user for a str of text and then outputs that same text but with all vowels (A, E, I, O, and U) omitted, whether inputted in uppercase or lowercase.
+
 import re
 
 def camel_to_snake(camel_case: str) -> str:
