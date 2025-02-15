cb2ba5ac1afe593664dc3a0940c15829de0a6e86
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-02-15 10:29:56 +0530

    Add Words Into Emojis

---
+++ Week6/Words_Into_Emojis.py
@@ -0,0 +1,3 @@
+# Week 6 Lab: : Words Into Emojis
+# Description: an interactive command-line interface (CLI) application using python that: translates words into emojis (including auto-filling mood-based emojis.
+
