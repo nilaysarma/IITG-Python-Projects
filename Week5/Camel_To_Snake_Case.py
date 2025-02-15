bd6c22d1d38f9701337c799edc029d798cca390c
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-02-15 08:39:26 +0530

    Modify Camel_To_Snake_Case.py

--- Week5/Camel_To_Snake_Case.py
+++ Week5/Camel_To_Snake_Case.py
@@ -2,14 +2,14 @@ import re
 
 def camel_to_snake(camel_case: str) -> str:
     if not camel_case[0].islower():
-        return "Invalid input. Please enter a camelCase variable name (starting with a lowercase letter)."
+        return "Invalid input. Please enter a camel case variable name (starting with a lowercase letter)."
 
     # Convert camelCase to snake_case
     snake_case = re.sub(r'([A-Z])', r'_\1', camel_case).lower()
     return snake_case
 
 # Get user input
-camel_case_input = input("Enter a camelCase variable name: ")
+camel_case_input = input("Enter a camel case variable name: ")
 
 # Check if the input is a valid camelCase format
 if not camel_case_input.isalnum() or "_" in camel_case_input:
