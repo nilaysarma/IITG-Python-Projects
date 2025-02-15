c9d7eeef070c7c84932a3e1a8d2c896ea47d36ed
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-02-15 10:59:06 +0530

    Add Language Detection

---
+++ Week6/Language_Detection.py
@@ -0,0 +1,3 @@
+# Week 6 Lab: Language Detection
+# Description: Create a small text sample (~50 sentences) by copy pasting translations from the following link. Note the 50 sentences should be drawn from multiple languages (e.g., English, Russian, Hindi, Chinese, Arabic, Spanish, French, German, Japanese, Korean, and more!). Let the resulting file.txt you thus created named as file.txt and this resides in your system.
+
