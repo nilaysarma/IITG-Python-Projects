02100e582b5da5034e1fa146f97600b5db2fa398
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-02-24 19:34:03 +0530

    Change Week7 py files to ipynb

--- Week7/File_Management_System.py
+++
@@ -1,3 +0,0 @@
-# Week 7 Lab: File Management System for a Research Lab
-# Description: You are working as a data manager in a research lab that collects and organizes data from various sources. Your task is to automate the organization of files stored in a messy directory containing text files (.txt), CSV datasets (.csv), JSON records (.json), images (.jpg, .png), and log files (.log).
-
--- Week7/Loading_Files.py
+++
@@ -1,3 +0,0 @@
-# Week 7 Lab: Experimenting with loading files
-# Description: You have joined a data analytics team that works with a variety of file formats. Your first task is to read, extract basic information, and print summaries from different types of files stored in a directory dataset.\
-
