190e503a4b3f7d9577fb87ed9b28d8291e091159
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-03-13 14:31:31 +0530

    Update mymathlibrary to 0.2.0

--- Week9/pypi/setup.py
+++ Week9/pypi/setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='mymathlibrary',
-    version='0.1.0',
+    version='0.2.0',
+    description='A simple python package to solve math problems.',
     packages=find_packages(),
     install_requires=[
         "numpy==2.2.3"
