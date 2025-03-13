0e36c6706d648ad46b610d6b3ee730e4551ccb96
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-03-13 15:11:07 +0530

    Official release mymathlibrary 1.0.0

--- Week9/pypi/setup.py
+++ Week9/pypi/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mymathlibrary',
-    version='0.2.0',
+    version='1.0.0',
     description='A simple python package to solve math problems.',
     packages=find_packages(),
     install_requires=[
